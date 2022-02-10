92ed61916b7a7f900712b3560e2503abb12d8252
Anamaria Huluba <82522372+anamaria-huluba@users.noreply.github.com>
2022-02-10 12:58:43 +0000

    Add files via upload

---
+++ LinearRegression - Honey_Production/script.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import linear_model
+
+df = pd.read_csv("https://content.codecademy.com/programs/data-science-path/linear_regression/honeyproduction.csv")
+
+#print(df.head(5))
+prod_per_year = df.groupby('year').totalprod.mean().reset_index()
+
+X = prod_per_year['year']
+X = X.values.reshape(-1, 1)
+
+y = prod_per_year['totalprod']
+plt.scatter(X, y)
+plt.show()
+
+regr = linear_model.LinearRegression()
+regr.fit(X, y)
+# print(regr.coef_)
+# print(regr.intercept_)
+
+y_predict = regr.predict(X)
+#print(y_predict)
+
+plt.plot(X, y_predict)
+plt.show()
+
+X_future = np.array(range(2013,2050))
+print(X_future)
+X_future = X_future.reshape(-1, 1)
+print(X_future)
+
+future_predict = regr.predict(X_future)
+plt.plot(X_future, future_predict)
+plt.show()
+
